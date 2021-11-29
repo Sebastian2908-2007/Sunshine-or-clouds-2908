ffe87e6091692016ac1d29c84971cd39b3bbb43e
Sebabin <sebastiancrespin279@gmail.com>
2021-11-28 17:12:32 -0700

    have weather for one and five day data coming back in one function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
 var getWeather = function(city) {
- var apiUrl ="//api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=dbc0a62496177eb4a5dc2b08dde0d7c4";
+ var apiUrl ="//api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=dbc0a62496177eb4a5dc2b08dde0d7c4&units=imperial";
+ var apiUrl2 = "//api.openweathermap.org/data/2.5/forecast?q="+ city +"&appid=dbc0a62496177eb4a5dc2b08dde0d7c4&units=imperial "
  fetch(apiUrl).then(function(response) {
      response.json().then(function(data){
          console.log(data);
      })
  });
+ fetch(apiUrl2).then(function(response) {
+    response.json().then(function(data) {
+        console.log(data);
+    });
+});
 };
+
+
 
 getWeather("salt lake");
