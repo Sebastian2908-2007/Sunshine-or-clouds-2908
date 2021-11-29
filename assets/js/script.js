1fe8248900ce633f21e87f7638d4b74c7ad78c91
Sebabin <sebastiancrespin279@gmail.com>
2021-11-28 18:29:49 -0700

    have one day data dynamically formatting on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
+
+
+
+var printOneDay = function(weather) {
+    var oneDay = document.querySelector("#one-day-forecast"); 
+    oneDay.innerHTML = "<h2 class='col'>" + weather.name + "</h2> <span class='col-12'>temp:"+ " " + weather.main.temp +"</span> <span class='col-12'>wind:" + " " + weather.wind.speed +"</span> <span class='col-12'>humidity:"+ " " + weather.main.humidity +"</span> <span class='col-12'>low temp:"+ " " + weather.main.temp_min +"</span>"
+    
+};
+
 var getWeather = function(city) {
  var apiUrl ="//api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=dbc0a62496177eb4a5dc2b08dde0d7c4&units=imperial";
  var apiUrl2 = "//api.openweathermap.org/data/2.5/forecast?q="+ city +"&appid=dbc0a62496177eb4a5dc2b08dde0d7c4&units=imperial "
  fetch(apiUrl).then(function(response) {
      response.json().then(function(data){
-         console.log(data);
+        printOneDay(data);
      })
  });
  fetch(apiUrl2).then(function(response) {
@@ -11,6 +20,7 @@ var getWeather = function(city) {
         console.log(data);
     });
 });
+
 };
 
 
