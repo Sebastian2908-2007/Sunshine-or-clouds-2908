0885b6948847be3cef5bcb204da26f4d5901c4b6
Sebabin <sebastiancrespin279@gmail.com>
2021-11-29 13:28:31 -0700

    have five day dynamically generating

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,12 @@ var printOneDay = function(weather) {
     
 };
 
+var printFiveDays = function(weather) {
+    var fiveDays = document.querySelector("#forecast-days");
+   fiveDays.innerHTML =
+ "<div class='col-2 one-day'><h6>"+ weather.list[0].dt_txt.split(" ")[0] +"</h6><span class='col-12 five-day-span'>Temp:" + " " + weather.list[0].main.temp +"</span><span class='col-12 five-day-span'>Wind:" + " " + weather.list[0].wind.speed +"</span><span class='col-12 five-day-span'>Humidity:" + " " + weather.list[0].main.humidity +"</span></div>" + "<div class='col-2 one-day'><h6>"+ weather.list[5].dt_txt.split(" ")[0] +"</h6><span class='col-12 five-day-span'>Temp:" + " " + weather.list[5].main.temp +"</span><span class='col-12 five-day-span'>Wind:" + " " + weather.list[5].wind.speed +"</span><span class='col-12 five-day-span'>Humidity:" + " " + weather.list[5].main.humidity +"</span></div>" + "<div class='col-2 one-day'><h6>"+ weather.list[10].dt_txt.split(" ")[0] +"</h6><span class='col-12 five-day-span'>Temp:" + " " + weather.list[10].main.temp +"</span><span class='col-12 five-day-span'>Wind:" + " " + weather.list[10].wind.speed +"</span><span class='col-12 five-day-span'>Humidity:" + " " + weather.list[10].main.humidity +"</span></div>" + "<div class='col-2 one-day'><h6>"+ weather.list[17].dt_txt.split(" ")[0] +"</h6><span class='col-12 five-day-span'>Temp:" + " " + weather.list[17].main.temp +"</span><span class='col-12 five-day-span'>Wind:" + " " + weather.list[17].wind.speed +"</span><span class='col-12 five-day-span'>Humidity:" + " " + weather.list[17].main.humidity +"</span></div>" + "<div class='col-2 one-day'><h6>"+ weather.list[25].dt_txt.split(" ")[0] +"</h6><span class='col-12 five-day-span'>Temp:" + " " + weather.list[25].main.temp +"</span><span class='col-12 five-day-span'>Wind:" + " " + weather.list[25].wind.speed +"</span><span class='col-12 five-day-span'>Humidity:" + " " + weather.list[25].main.humidity +"</span></div>"
+};
+
 var getWeather = function(city) {
  var apiUrl ="//api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=dbc0a62496177eb4a5dc2b08dde0d7c4&units=imperial";
  var apiUrl2 = "//api.openweathermap.org/data/2.5/forecast?q="+ city +"&appid=dbc0a62496177eb4a5dc2b08dde0d7c4&units=imperial "
@@ -18,6 +24,7 @@ var getWeather = function(city) {
  fetch(apiUrl2).then(function(response) {
     response.json().then(function(data) {
         console.log(data);
+       printFiveDays(data)
     });
 });
 
@@ -25,4 +32,4 @@ var getWeather = function(city) {
 
 
 
-getWeather("salt lake");
+getWeather("billings");
